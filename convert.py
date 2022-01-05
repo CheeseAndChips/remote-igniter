d31cd2e07c1ab7d7879a87270273f00a24948566
Joris Pevcevicius <joris@pevcas.eu>
2022-01-05 22:44:51 +0200

    major js overhaul

--- convert.py
+++ convert.py
@@ -2,6 +2,7 @@ import subprocess
 
 formstr = '#ifndef WEB_H\n#define WEB_H\nconst char web[] = {{{}}};\n#endif'
 outp = subprocess.run(['minify', './src/index.html'], capture_output=True).stdout
+# outp = subprocess.run(['cat', './src/index.html'], capture_output=True).stdout
 final_output = formstr.format(str(['0x' + bytes([i]).hex() for i in outp]).replace('\'', '')[1:-1])
 
 with open('./include/web_include.h', 'w') as f:
